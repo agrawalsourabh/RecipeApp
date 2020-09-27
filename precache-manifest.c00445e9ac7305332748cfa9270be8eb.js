5b9e735beefbd3e19aa8faf023d98d2f1ac4e921
Sourabh Agrawal <agrawal.sourabh644@gmail.com>
2020-09-27 21:32:31 +0530

    Updates

---
+++ precache-manifest.c00445e9ac7305332748cfa9270be8eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa1c044af5f05cfedb27fa2c4c8fdc07",
+    "url": "/RecipeApp/index.html"
+  },
+  {
+    "revision": "50b2a0fb27e33157edf5",
+    "url": "/RecipeApp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8de5dbd36aa9f294e734",
+    "url": "/RecipeApp/static/css/main.631fc424.chunk.css"
+  },
+  {
+    "revision": "50b2a0fb27e33157edf5",
+    "url": "/RecipeApp/static/js/2.5d0c5289.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RecipeApp/static/js/2.5d0c5289.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8de5dbd36aa9f294e734",
+    "url": "/RecipeApp/static/js/main.1a9f9354.chunk.js"
+  },
+  {
+    "revision": "2569b9ec1ba1142dccbc",
+    "url": "/RecipeApp/static/js/runtime-main.8f6c811b.js"
+  }
+]);
